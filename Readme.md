65d24768017bad995f50e168efdff446ee08ddce
pgorade9 <prashant.gorade@gmail.com>
2024-10-26 16:43:47 +0530

    Add Readme and update postgres dag

---
+++ Readme.md
@@ -0,0 +1,3 @@
+1. Use command **docker compose up airflow-init** to start airflow (this will start postgres and redis)
+2. Note the hyphen in airflow-init
+3. Next use command **docker compose up** to start airflow (this will start remaining airflow  components)
--- dags/post_gres.py
+++ dags/post_gres.py
@@ -1,7 +1,13 @@
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 dag = DAG("post_gres_dag")
 
-create_table = PostgresOperator(task_id="create_table", postgres_conn_id="pg_db",sql="""CREATE TABLE IF NOT 
-EXISTS my_books( id SERIAL PRIMARY KEY, title TEXT NOT NULL, pages INT);""",dag=dag)
+create_table = PostgresOperator(task_id="create_table", postgres_conn_id="pg_db", sql="""CREATE TABLE IF NOT 
+EXISTS my_books( id SERIAL PRIMARY KEY, title TEXT NOT NULL, pages INT);""", dag=dag)
+
+fetch_books = PostgresOperator(task_id="make_entry", postgres_conn_id="pg_db", sql="""INSERT INTO my_books 
+VALUES ( 1, 'REST API Fundamentals', 100);""", dag=dag)
+
+create_table >> fetch_books
